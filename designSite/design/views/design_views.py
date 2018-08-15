7e55b253b4ea59d4b57cd8cb33e7d77ae48aaad6
xwy27 <xuwy27@mail2.sysu.edu.cn>
2018-08-16 00:39:41 +0800

    add json to sbol code

--- designSite/design/views/design_views.py
+++ designSite/design/views/design_views.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 import os
 from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from sbol import *
 
 import json
 from django.http import HttpResponse, JsonResponse
@@ -33,6 +35,9 @@ All response contains a status in json
 
 def design(request):
     return render(request, 'design.html')
+
+def test(request):
+    return render(request, 'test.html')
 
 # Favorites related views
 
@@ -558,3 +563,167 @@ def plasmid_data(request):
     
     with open(os.path.join(BASE_DIR, 'tools/plasmidData.json')) as f:
         return JsonResponse({ 'data': json.load(f) })
+
+from django.views.decorators.csrf import csrf_exempt
+@csrf_exempt
+def get_sbol_doc(request):
+    if request.method == 'POST':
+        data = json.loads(request.POST['data'])
+        # data = json.load(test)
+
+        setHomespace('http://sys-bio.org')
+        doc = Document()
+
+        roles = {
+            'DNA': SO_GENE,
+            'RNA': SO_SGRNA,
+            'Promoter': SO_PROMOTER,
+            'RBS': SO_RBS,
+            'CDS': SO_CDS,
+            'Terminator': SO_TERMINATOR
+        }
+
+
+        # test = open("data.json", encoding='utf-8')
+        # data = json.load(test)
+
+        activity = Activity(data['circuit']['name'])
+        activity.displayId = data['circuit']['name']
+        activity.description = data['circuit']['description']
+        doc.addActivity(activity)
+
+        # load components
+        components = {}
+        for component in data['components']:
+            temp = ComponentDefinition(component['id'])
+            temp.roles = roles[component['role']]
+            temp.description = component['description']
+            temp.sequence = Sequence(component['id'], component['sequence'])
+            components[component['id']] = temp
+            doc.addComponentDefinition(temp)
+
+        # load line structure
+        for line in data['lines']:
+            structure = []
+            circuit = ComponentDefinition(line['name'])
+            doc.addComponentDefinition(circuit)
+            for temp in line['structure']:
+                structure.append(components[temp])
+            circuit.assemblePrimaryStructure(structure)
+
+        # functional components
+        pro_fcs = {}
+        inh_fcs = {}
+        com_fcs = {}
+
+        # promotion
+        if 'promotions' in data.keys():
+            proModule = ModuleDefinition('promotion_module')
+            doc.addModuleDefinition(proModule)
+
+            for index, promotion in enumerate(data['promotions']):
+                stimulatorName = promotion['stimulator']
+                otherName = promotion['other']
+                
+                if not stimulatorName in pro_fcs.keys():
+                    stimulator_fc = proModule.functionalComponents.create(stimulatorName)
+                    stimulator_fc.definition = components[stimulatorName].identity
+                    stimulator_fc.access = SBOL_ACCESS_PUBLIC
+                    stimulator_fc.direction = SBOL_DIRECTION_IN_OUT
+                    pro_fcs[stimulatorName] = stimulator_fc
+
+                if not otherName in pro_fcs.keys():
+                    other_fc = proModule.functionalComponents.create(otherName)
+                    other_fc.definition = components[otherName].identity
+                    other_fc.access = SBOL_ACCESS_PUBLIC
+                    other_fc.direction = SBOL_DIRECTION_IN_OUT
+                    pro_fcs[otherName] = other_fc
+
+                proInteraction = proModule.interactions.create('promotion_' + str(index))
+                proInteraction.types = SBO_STIMULATION
+
+                stimulator_participation = proInteraction.participations.create(stimulatorName)
+                stimulator_participation.roles = SBO_STIMULATOR
+                stimulator_participation.participant = pro_fcs[stimulatorName].identity
+
+                other_participation = proInteraction.participations.create(otherName)
+                other_participation.roles = components[otherName].roles
+                other_participation.participant = pro_fcs[otherName].identity
+
+        # inhibition
+        if 'inhibitions' in data.keys():
+            inhModule = ModuleDefinition('inhbition_module')
+            doc.addModuleDefinition(inhModule)
+
+            for index, inhibition in enumerate(data['inhibitions']):
+                inhibitorName = inhibition['inhibitor']
+                otherName = inhibition['other']
+                
+                if not inhibitorName in inh_fcs.keys():
+                    inhibitor_fc = inhModule.functionalComponents.create(inhibitorName)
+                    inhibitor_fc.definition = components[inhibitorName].identity
+                    inhibitor_fc.access = SBOL_ACCESS_PUBLIC
+                    inhibitor_fc.direction = SBOL_DIRECTION_IN_OUT
+                    inh_fcs[inhibitorName] = inhibitor_fc
+
+                if not otherName in inh_fcs.keys():
+                    other_fc = inhModule.functionalComponents.create(otherName)
+                    other_fc.definition = components[otherName].identity
+                    other_fc.access = SBOL_ACCESS_PUBLIC
+                    other_fc.direction = SBOL_DIRECTION_IN_OUT
+                    inh_fcs[otherName] = other_fc
+
+                inhInteraction = inhModule.interactions.create('inhibition_' + str(index))
+                inhInteraction.types = SBO_INHIBITION
+
+                inhibitor_participation = inhInteraction.participations.create(inhibitorName)
+                inhibitor_participation.roles = SBO_INHIBITOR
+                inhibitor_participation.participant = inh_fcs[inhibitorName].identity
+
+                other_participation = inhInteraction.participations.create(otherName)
+                other_participation.roles = components[otherName].roles
+                other_participation.participant = inh_fcs[otherName].identity
+
+        # combination
+        if 'combinations' in data.keys():
+            comModule = ModuleDefinition('combination_module')
+            doc.addModuleDefinition(comModule)
+
+            for index, combination in enumerate(data['combinations']):
+                comInteraction = comModule.interactions.create('combination_' + str(index))
+                comInteraction.role = SBO_NONCOVALENT_BINDING
+                for reactant in combination['reactants']:
+                    if not reactant in com_fcs.keys():
+                        reactant_fc = comModule.functionalComponents.create(reactant)
+                        reactant_fc.definition = components[reactant].identity
+                        reactant_fc.access = SBOL_ACCESS_PUBLIC
+                        reactant_fc.direction = SBOL_DIRECTION_IN_OUT
+                        com_fcs[reactant] = reactant_fc
+                    reactant_participation = comInteraction.participations.create(reactant)
+                    reactant_participation.roles = SBO_REACTANT
+                    reactant_participation.participant = com_fcs[reactant].identity
+                
+                product = combination['product']
+                if not product in com_fcs.keys():
+                    product_fc = comModule.functionalComponents.create(product)
+                    product_fc.definition = components[product].identity
+                    product_fc.access = SBOL_ACCESS_PUBLIC
+                    product_fc.direction = SBOL_DIRECTION_IN_OUT
+                    com_fcs[product] = product_fc
+                    product_participation = comInteraction.participations.create(product)
+                    product_participation.roles = SBO_PRODUCT
+                    product_participation.participant = com_fcs[product].identity
+            
+
+        # create sbol document
+        circuit_name = data['circuit']['name']
+        filename = circuit_name + '.xml'
+        result = doc.write(filename)
+        print(result)
+
+        response = HttpResponse(open(filename,"rb"),content_type="text/xml")
+
+        if os.path.exists(filename):
+            os.remove(filename)
+
+        return response
