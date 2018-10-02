7c640e394fdce61a714b53bc4ba5234978ef00d3
kingiw <kingiw@hotmail.com>
2018-10-03 01:43:34 +0800

    trivial

--- designSite/search/views/search_views.py
+++ designSite/search/views/search_views.py
@@ -33,88 +33,89 @@ def search(request):
 
 @login_required
 def work(request):
-    if request.method == 'POST':
-        return
-    try:
-        wk = Works.objects.get(TeamID = request.GET.get('id'))
-        use_parts = wk.Use_parts.split(';')
-        part = []
-        for item in use_parts:
-            try:
-                pt = Parts.objects.get(Name = item)
-                if request.user.is_authenticated:
-                    try:
-                        FavoriteParts.objects.get(user = request.user, part = pt)
-                        favor = True
-                    except FavoriteParts.DoesNotExist:
-                        favor = False
-                else:
-                    favor = False
+  return render(request, 'work.html')
+    # if request.method == 'POST':
+    #     return
+    # try:
+    #     wk = Works.objects.get(TeamID = request.GET.get('id'))
+    #     use_parts = wk.Use_parts.split(';')
+    #     part = []
+    #     for item in use_parts:
+    #         try:
+    #             pt = Parts.objects.get(Name = item)
+    #             if request.user.is_authenticated:
+    #                 try:
+    #                     FavoriteParts.objects.get(user = request.user, part = pt)
+    #                     favor = True
+    #                 except FavoriteParts.DoesNotExist:
+    #                     favor = False
+    #             else:
+    #                 favor = False
 
-                part.append({
-                    'id': pt.id,
-                    'BBa': item,
-                    'name': pt.secondName,
-                    'isFavourite': favor})
+    #             part.append({
+    #                 'id': pt.id,
+    #                 'BBa': item,
+    #                 'name': pt.secondName,
+    #                 'isFavourite': favor})
 
 
-            except Parts.DoesNotExist:
-                part.append({
-                    'id': request.GET.get('id'),
-                    'BBa': item,
-                    'name': pt.secondName,
-                    'isFavourite': False})
-        if request.user.is_authenticated:
-            try:
-                UserFavorite.objects.get(user = request.user, circuit = wk.Circuit)
-                favorite = True
-            except UserFavorite.DoesNotExist:
-                favorite = False
-        else:
-            favorite = False
+    #         except Parts.DoesNotExist:
+    #             part.append({
+    #                 'id': request.GET.get('id'),
+    #                 'BBa': item,
+    #                 'name': pt.secondName,
+    #                 'isFavourite': False})
+    #     if request.user.is_authenticated:
+    #         try:
+    #             UserFavorite.objects.get(user = request.user, circuit = wk.Circuit)
+    #             favorite = True
+    #         except UserFavorite.DoesNotExist:
+    #             favorite = False
+    #     else:
+    #         favorite = False
 
-        if wk.Img.all().count() == 0:
-            Img = [wk.DefaultImg]
-        else:
-            Img = [i.URL for i in wk.Img.all()]
+    #     if wk.Img.all().count() == 0:
+    #         Img = [wk.DefaultImg]
+    #     else:
+    #         Img = [i.URL for i in wk.Img.all()]
 
-        Awards = wk.Award.split(';')
-        while len(Awards) > 0 and Awards[-1] == '':
-            Awards = Awards[: -1]
+    #     Awards = wk.Award.split(';')
+    #     while len(Awards) > 0 and Awards[-1] == '':
+    #         Awards = Awards[: -1]
         
-        relatedTeams = Trelation.objects.filter(first = wk)
-        relatedTeams = list(map(lambda rt: {
-            'teamName': rt.second.Teamname,
-            'projectName': rt.second.Title,
-            'year': rt.second.Year,
-            'id': rt.second.TeamID
-        }, relatedTeams))
+    #     relatedTeams = Trelation.objects.filter(first = wk)
+    #     relatedTeams = list(map(lambda rt: {
+    #         'teamName': rt.second.Teamname,
+    #         'projectName': rt.second.Title,
+    #         'year': rt.second.Year,
+    #         'id': rt.second.TeamID
+    #     }, relatedTeams))
 
-        keywords = TeamKeyword.objects.filter(Team = wk)
-        keywords = list(map(lambda tk: [tk.keyword, tk.score], keywords))
-        keywords.sort(key = lambda tk: -tk[1])
-        keywords = list(map(lambda tk: tk[0], keywords))[:5]
+    #     keywords = TeamKeyword.objects.filter(Team = wk)
+    #     keywords = list(map(lambda tk: [tk.keyword, tk.score], keywords))
+    #     keywords.sort(key = lambda tk: -tk[1])
+    #     keywords = list(map(lambda tk: tk[0], keywords))[:5]
 
-        wk.ReadCount += 1
-        wk.save()
-        context = {
-            'projectName': wk.Title,
-            'teamName': wk.Teamname,
-            'year': wk.Year,
-            'readCount': wk.ReadCount,
-            'medal': wk.Medal,
-            'rewards': Awards,
-            'description': wk.Description,
-            'isFavourite': favorite,
-            'images': Img,
-            'designId': -1 if wk.Circuit is None else wk.Circuit.id,
-            'part': part,
-            'logo': wk.logo,
-            'keywords': keywords,
-            'relatedTeams': relatedTeams
-        }
+    #     wk.ReadCount += 1
+    #     wk.save()
+    #     context = {
+    #         'projectName': wk.Title,
+    #         'teamName': wk.Teamname,
+    #         'year': wk.Year,
+    #         'readCount': wk.ReadCount,
+    #         'medal': wk.Medal,
+    #         'rewards': Awards,
+    #         'description': wk.Description,
+    #         'isFavourite': favorite,
+    #         'images': Img,
+    #         'designId': -1 if wk.Circuit is None else wk.Circuit.id,
+    #         'part': part,
+    #         'logo': wk.logo,
+    #         'keywords': keywords,
+    #         'relatedTeams': relatedTeams
+    #     }
 
-        return render(request, 'work.html', context)
+    #     return render(request, 'work.html', context)
 
-    except Works.DoesNotExist:
-        return HttpResponse("Work Does Not Exist!")
+    # except Works.DoesNotExist:
+    #     return HttpResponse("Work Does Not Exist!")
