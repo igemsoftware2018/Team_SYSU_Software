c57979b93679ff43ac8604976e09ac53a604f895
kingiw <kingiw@hotmail.com>
2018-10-14 16:12:50 +0800

    Update the naming principle of part

--- designSite/opt_sim_module/solve.py
+++ designSite/opt_sim_module/solve.py
@@ -1,8 +1,8 @@
 # import matplotlib.pyplot as plt
-import numpy as np
 # from mpl_toolkits.mplot3d import Axes3D
-from scipy import cos, linspace, pi, sin
+from scipy import sin, cos, linspace, pi
 from scipy.integrate import odeint, solve_bvp, solve_ivp
+import numpy as np
 
 
 def solve_ode(data, k, evol_t):
@@ -11,23 +11,27 @@ def solve_ode(data, k, evol_t):
 
 	def fvdp(y,t):
 		Dy = []
-		for i in range(n):
-			inhibition = 1.0
-			promotion = 0.0
-
-			#inhitbition & promotion
-			for j in range(n):
-				if data['matrix'][i][j] == 1:
-					promotion += y[j]
-				elif data['matrix'][i][j] == 0:
-					continue
-				else:
-					inhibition *= (1.0/(1 + y[j]**data['n'][j]))
-			dy = k[i] * (promotion * inhibition) - data['d'][i] * y[i]
-
-			#combine
-
+		if len(data['matrix']) == 1:
+			dy = k[0] - data['d'][0] * y[0]
 			Dy.append(dy)
+		else:
+			for i in range(n):
+				inhibition = 1.0
+				promotion = 0.0
+
+				#inhitbition & promotion
+				for j in range(n):
+					if data['matrix'][i][j] == 1:
+						promotion += y[j]
+					elif data['matrix'][i][j] == 0:
+						continue
+					else:
+						inhibition *= (1.0/(1 + y[j]**data['n'][j]))
+				dy = k[i] * (promotion * inhibition) - data['d'][i] * y[i]
+
+				#combine
+
+				Dy.append(dy)
 
 		return np.array(Dy)
 
