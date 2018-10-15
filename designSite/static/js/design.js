b598f8ea4a71a697de74f14c8423a6bd5854c295
某罐头菠萝 <heglan@mail2.sysu.edu.cn>
2018-10-16 00:51:42 +0800

    add simulation loading

--- designSite/static/js/design.js
+++ designSite/static/js/design.js
@@ -2290,14 +2290,25 @@ function submitSimulation() {
         }
     });
     if (checkFlag) {
-        $.post({
+        $design_msg_modal.modal('show');
+        $design_msg_body.text('Generating simulation result.');
+        $.ajax({
             url: `/api/simulation`,
+            method: "POST",
             data: {
                 'data': JSON.stringify(submitData),
                 csrfmiddlewaretoken: $('[name=csrfmiddlewaretoken]').val()
+            },
+            success: function (data) {
+                $design_msg_modal.modal('hide');
+                showSimulationChart(data);
+            },
+            error: function() {
+                $design_msg_body.text('ERROR. Invaild design or value.');
+                setTimeout(() => {
+                    $design_msg_modal.modal('hide');
+                }, 1000);
             }
-        }, (data) => {
-            showSimulationChart(data);
         });
     }
 }
